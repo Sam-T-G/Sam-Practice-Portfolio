063d4b48fd9b9f82a3c4b698ad498e09e6752852
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2024-03-05 11:42:43 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <div className="app">
         <Routes>
-          <Route></Route>
+          <Route
+            path="/"
+            element={<Home title="Sam Gerungan" subtitle="" heroImage="" />}
+          />
         </Routes>
       </div>
     </BrowserRouter>
