c480cb6ec2ce884001e2a003c76470e928845c79
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2024-03-05 11:44:43 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Navigation from "./components/Navigation";
 import Home from "./components/Home";
+import Projects from "./components/Projects";
+import About from "./components/About";
+import Footer from "./components/Footer";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <div className="app">
         <Routes>
+          <Navigation />
           <Route
             path="/"
             element={<Home title="Sam Gerungan" subtitle="" heroImage="" />}
           />
         </Routes>
+        <Footer />
       </div>
     </BrowserRouter>
   );
