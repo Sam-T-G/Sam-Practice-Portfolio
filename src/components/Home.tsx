70a89a43e12fb5f3b8bbb72b25572de8b9cae3de
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2024-03-05 11:36:24 -0800

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./Home.css";
 
-const Home: React.FC = () => {
-  return <div className="home-container"></div>;
+interface HomeProps {
+  title: string;
+  subtitle: string;
+  heroImage: string;
+}
+
+const Home: React.FC<HomeProps> = () => {
+  return (
+    <div className="home-container">
+      <div className="home-content">
+        <div className="hero-element"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
