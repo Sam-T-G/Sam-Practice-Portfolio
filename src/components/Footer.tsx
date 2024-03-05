8075dcd3105d4d4d4b5a6f5374f9d2fd36e0ffff
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2024-03-05 12:13:41 -0800

    footer base styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./Footer.css";
+import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => {
-  return <div className="footer-container"></div>;
+  return (
+    <div className="footer-container">
+      <ul className="footer-menu">
+        <li className="footer-button">
+          <Link to="/home">Home</Link>
+        </li>
+        <li className="footer-button">
+          <Link to="/home">Projects</Link>
+        </li>
+        <li className="navigation-button">
+          <Link to="/footer">About</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Footer;
